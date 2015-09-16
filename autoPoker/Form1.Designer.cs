ff8673bb7b29bc7ecee26cdaf55383d61659fa63
Seongsu Kim <seongsuk@buffalo.edu>
2015-09-16 16:37:04 -0400

    First Commit
    First Commit

---
+++ autoPoker/Form1.Designer.cs
@@ -0,0 +1,418 @@
+﻿namespace autoPoker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadButton = new System.Windows.Forms.Button();
+            this.loadStatus = new System.Windows.Forms.Label();
+            this.loadBox = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.check_button = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.bigImage = new System.Windows.Forms.Button();
+            this.r1 = new System.Windows.Forms.Label();
+            this.r2 = new System.Windows.Forms.Label();
+            this.r3 = new System.Windows.Forms.Label();
+            this.r4 = new System.Windows.Forms.Label();
+            this.r5 = new System.Windows.Forms.Label();
+            this.loadCard = new System.Windows.Forms.Button();
+            this.smallShape = new System.Windows.Forms.Button();
+            this.bigShape = new System.Windows.Forms.Button();
+            this.r10 = new System.Windows.Forms.Label();
+            this.r9 = new System.Windows.Forms.Label();
+            this.r8 = new System.Windows.Forms.Label();
+            this.r7 = new System.Windows.Forms.Label();
+            this.r6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.handBig = new System.Windows.Forms.Button();
+            this.r13 = new System.Windows.Forms.Label();
+            this.r11 = new System.Windows.Forms.Label();
+            this.r14 = new System.Windows.Forms.Label();
+            this.r12 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.loadBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(28, 48);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(109, 29);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "Load game";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // loadStatus
+            // 
+            this.loadStatus.AutoSize = true;
+            this.loadStatus.Location = new System.Drawing.Point(16, 21);
+            this.loadStatus.Name = "loadStatus";
+            this.loadStatus.Size = new System.Drawing.Size(131, 15);
+            this.loadStatus.TabIndex = 1;
+            this.loadStatus.Text = "Game : Not loaded";
+            // 
+            // loadBox
+            // 
+            this.loadBox.Controls.Add(this.loadStatus);
+            this.loadBox.Controls.Add(this.loadButton);
+            this.loadBox.Location = new System.Drawing.Point(12, 12);
+            this.loadBox.Name = "loadBox";
+            this.loadBox.Size = new System.Drawing.Size(172, 90);
+            this.loadBox.TabIndex = 2;
+            this.loadBox.TabStop = false;
+            this.loadBox.Text = "1. Load Game";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // check_button
+            // 
+            this.check_button.Location = new System.Drawing.Point(31, 147);
+            this.check_button.Name = "check_button";
+            this.check_button.Size = new System.Drawing.Size(75, 23);
+            this.check_button.TabIndex = 7;
+            this.check_button.Text = "Check";
+            this.check_button.UseVisualStyleBackColor = true;
+            this.check_button.Click += new System.EventHandler(this.check_button_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 116);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(39, 25);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(76, 116);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(39, 25);
+            this.textBox2.TabIndex = 9;
+            // 
+            // bigImage
+            // 
+            this.bigImage.Location = new System.Drawing.Point(212, 156);
+            this.bigImage.Name = "bigImage";
+            this.bigImage.Size = new System.Drawing.Size(75, 23);
+            this.bigImage.TabIndex = 10;
+            this.bigImage.Text = "bigImage";
+            this.bigImage.UseVisualStyleBackColor = true;
+            this.bigImage.Click += new System.EventHandler(this.bigImage_Click);
+            // 
+            // r1
+            // 
+            this.r1.AutoSize = true;
+            this.r1.Location = new System.Drawing.Point(209, 81);
+            this.r1.Name = "r1";
+            this.r1.Size = new System.Drawing.Size(0, 15);
+            this.r1.TabIndex = 11;
+            // 
+            // r2
+            // 
+            this.r2.AutoSize = true;
+            this.r2.Font = new System.Drawing.Font("Gulim", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.r2.Location = new System.Drawing.Point(229, 81);
+            this.r2.Name = "r2";
+            this.r2.Size = new System.Drawing.Size(0, 15);
+            this.r2.TabIndex = 12;
+            // 
+            // r3
+            // 
+            this.r3.AutoSize = true;
+            this.r3.Font = new System.Drawing.Font("Gulim", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.r3.Location = new System.Drawing.Point(249, 81);
+            this.r3.Name = "r3";
+            this.r3.Size = new System.Drawing.Size(0, 15);
+            this.r3.TabIndex = 13;
+            // 
+            // r4
+            // 
+            this.r4.AutoSize = true;
+            this.r4.Font = new System.Drawing.Font("Gulim", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.r4.Location = new System.Drawing.Point(269, 81);
+            this.r4.Name = "r4";
+            this.r4.Size = new System.Drawing.Size(0, 15);
+            this.r4.TabIndex = 14;
+            // 
+            // r5
+            // 
+            this.r5.AutoSize = true;
+            this.r5.Font = new System.Drawing.Font("Gulim", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.r5.Location = new System.Drawing.Point(289, 81);
+            this.r5.Name = "r5";
+            this.r5.Size = new System.Drawing.Size(0, 15);
+            this.r5.TabIndex = 15;
+            // 
+            // loadCard
+            // 
+            this.loadCard.Location = new System.Drawing.Point(294, 156);
+            this.loadCard.Name = "loadCard";
+            this.loadCard.Size = new System.Drawing.Size(75, 23);
+            this.loadCard.TabIndex = 16;
+            this.loadCard.Text = "loadCard";
+            this.loadCard.UseVisualStyleBackColor = true;
+            this.loadCard.Click += new System.EventHandler(this.loadCard_Click);
+            // 
+            // smallShape
+            // 
+            this.smallShape.Location = new System.Drawing.Point(212, 186);
+            this.smallShape.Name = "smallShape";
+            this.smallShape.Size = new System.Drawing.Size(103, 25);
+            this.smallShape.TabIndex = 17;
+            this.smallShape.Text = "smallShape";
+            this.smallShape.UseVisualStyleBackColor = true;
+            this.smallShape.Click += new System.EventHandler(this.smallShape_Click);
+            // 
+            // bigShape
+            // 
+            this.bigShape.Location = new System.Drawing.Point(212, 217);
+            this.bigShape.Name = "bigShape";
+            this.bigShape.Size = new System.Drawing.Size(103, 23);
+            this.bigShape.TabIndex = 18;
+            this.bigShape.Text = "bigShape";
+            this.bigShape.UseVisualStyleBackColor = true;
+            this.bigShape.Click += new System.EventHandler(this.bigShape_Click);
+            // 
+            // r10
+            // 
+            this.r10.AutoSize = true;
+            this.r10.Location = new System.Drawing.Point(288, 111);
+            this.r10.Name = "r10";
+            this.r10.Size = new System.Drawing.Size(0, 15);
+            this.r10.TabIndex = 23;
+            // 
+            // r9
+            // 
+            this.r9.AutoSize = true;
+            this.r9.Location = new System.Drawing.Point(268, 111);
+            this.r9.Name = "r9";
+            this.r9.Size = new System.Drawing.Size(0, 15);
+            this.r9.TabIndex = 22;
+            // 
+            // r8
+            // 
+            this.r8.AutoSize = true;
+            this.r8.Location = new System.Drawing.Point(248, 111);
+            this.r8.Name = "r8";
+            this.r8.Size = new System.Drawing.Size(0, 15);
+            this.r8.TabIndex = 21;
+            // 
+            // r7
+            // 
+            this.r7.AutoSize = true;
+            this.r7.Location = new System.Drawing.Point(228, 111);
+            this.r7.Name = "r7";
+            this.r7.Size = new System.Drawing.Size(0, 15);
+            this.r7.TabIndex = 20;
+            // 
+            // r6
+            // 
+            this.r6.AutoSize = true;
+            this.r6.Location = new System.Drawing.Point(208, 111);
+            this.r6.Name = "r6";
+            this.r6.Size = new System.Drawing.Size(0, 15);
+            this.r6.TabIndex = 19;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "label2";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(142, 273);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 25);
+            this.textBox3.TabIndex = 25;
+            // 
+            // handBig
+            // 
+            this.handBig.Location = new System.Drawing.Point(336, 187);
+            this.handBig.Name = "handBig";
+            this.handBig.Size = new System.Drawing.Size(75, 23);
+            this.handBig.TabIndex = 26;
+            this.handBig.Text = "handBig";
+            this.handBig.UseVisualStyleBackColor = true;
+            this.handBig.Click += new System.EventHandler(this.handBig_Click);
+            // 
+            // r13
+            // 
+            this.r13.AutoSize = true;
+            this.r13.Font = new System.Drawing.Font("Gulim", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.r13.Location = new System.Drawing.Point(235, 46);
+            this.r13.Name = "r13";
+            this.r13.Size = new System.Drawing.Size(0, 15);
+            this.r13.TabIndex = 28;
+            // 
+            // r11
+            // 
+            this.r11.AutoSize = true;
+            this.r11.Font = new System.Drawing.Font("Gulim", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.r11.Location = new System.Drawing.Point(215, 46);
+            this.r11.Name = "r11";
+            this.r11.Size = new System.Drawing.Size(0, 15);
+            this.r11.TabIndex = 27;
+            // 
+            // r14
+            // 
+            this.r14.AutoSize = true;
+            this.r14.Font = new System.Drawing.Font("Gulim", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.r14.Location = new System.Drawing.Point(234, 17);
+            this.r14.Name = "r14";
+            this.r14.Size = new System.Drawing.Size(0, 15);
+            this.r14.TabIndex = 30;
+            // 
+            // r12
+            // 
+            this.r12.AutoSize = true;
+            this.r12.Font = new System.Drawing.Font("Gulim", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.r12.Location = new System.Drawing.Point(214, 17);
+            this.r12.Name = "r12";
+            this.r12.Size = new System.Drawing.Size(0, 15);
+            this.r12.TabIndex = 29;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(142, 304);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 25);
+            this.textBox4.TabIndex = 31;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Gulim", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(347, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "Start bot";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Gulim", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2.Location = new System.Drawing.Point(347, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 23);
+            this.button2.TabIndex = 33;
+            this.button2.Text = "Stop bot";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 368);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.r14);
+            this.Controls.Add(this.r12);
+            this.Controls.Add(this.r13);
+            this.Controls.Add(this.r11);
+            this.Controls.Add(this.handBig);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.r10);
+            this.Controls.Add(this.r9);
+            this.Controls.Add(this.r8);
+            this.Controls.Add(this.r7);
+            this.Controls.Add(this.r6);
+            this.Controls.Add(this.bigShape);
+            this.Controls.Add(this.smallShape);
+            this.Controls.Add(this.loadCard);
+            this.Controls.Add(this.r5);
+            this.Controls.Add(this.r4);
+            this.Controls.Add(this.r3);
+            this.Controls.Add(this.r2);
+            this.Controls.Add(this.r1);
+            this.Controls.Add(this.bigImage);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.check_button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loadBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.loadBox.ResumeLayout(false);
+            this.loadBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Label loadStatus;
+        private System.Windows.Forms.GroupBox loadBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button check_button;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button bigImage;
+        private System.Windows.Forms.Label r1;
+        private System.Windows.Forms.Label r2;
+        private System.Windows.Forms.Label r3;
+        private System.Windows.Forms.Label r4;
+        private System.Windows.Forms.Label r5;
+        private System.Windows.Forms.Button loadCard;
+        private System.Windows.Forms.Button smallShape;
+        private System.Windows.Forms.Button bigShape;
+        private System.Windows.Forms.Label r10;
+        private System.Windows.Forms.Label r9;
+        private System.Windows.Forms.Label r8;
+        private System.Windows.Forms.Label r7;
+        private System.Windows.Forms.Label r6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button handBig;
+        private System.Windows.Forms.Label r13;
+        private System.Windows.Forms.Label r11;
+        private System.Windows.Forms.Label r14;
+        private System.Windows.Forms.Label r12;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
